4c50865c70a7482bab2e573f000fb27f5b92263f
Gustavo Motta <gustavo.motta@autbank.com.br>
2024-04-16 21:39:20 -0300

    feat(setup): setup sdk and NOT INSTALL VPN PLEASE

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from time import sleep
 import threading
+import os
+
+BASEDIR = "./setup"
+SDKMAN_SCRIPT = 'curl -s "https://get.sdkman.io" | bash'
+VPN_PACKAGES_APT_SCRIPT = 'sudo apt-get install network-manager-l2tp network-manager-l2tp-gnome apt-transport-https ca-certificates libreswan -y'
 
 setup = False
 
@@ -9,11 +14,33 @@ def loadIcon():
             sleep(0.1)
             print(' Loading ' +i, end = '\r')
             if setup:
-                 print("\r\n Done!")
+                 print("\r\n Done!" )
                  break
 
 def initSetup():
-    sleep(5)
+    
+    setupSdkMan()
+    setupL2tpVpnPackages()
+    
+    try:
+        os.mkdir(BASEDIR)
+    except FileExistsError:
+        print("The setup path aready exist")
+    sleep(2)
+
+def setupL2tpVpnPackages():
+    try:
+        os.system(VPN_PACKAGES_APT_SCRIPT)
+        print("L2tp packages instaled")
+    except SystemError:
+        print("error to setup vpn packages")
+
+def setupSdkMan():
+    try:
+        os.system(SDKMAN_SCRIPT)
+        print("sdkman installed")
+    except SystemError:
+        print("Error to setup SdkMan")
 
 def main():
     print("Setup develop tools!")
